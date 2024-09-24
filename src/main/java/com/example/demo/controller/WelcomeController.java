2eca01e333d3d86535b1146f291d21ec81f3e481
lucasmodin <lumo0002@stud.kea.dk>
2024-09-24 22:13:36 +0200

    added welcome.html and welcome message

---
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcome() {
+        String welcomeMessage = "Hello World!";
+        return new ResponseEntity<>(welcomeMessage, HttpStatus.OK);
+    }
+
+}
